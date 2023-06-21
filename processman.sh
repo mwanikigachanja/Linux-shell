b1a50e087e0bd52f2b1179585e535cc4d6cd3b4b
Charles Mwaniki <mwanikigachanja@gmail.com>
2023-06-21 12:22:03 +0300

    Process Management

---
+++ processman.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# List all running processes
+ps aux
+
+# Search for a specific process
+pgrep firefox
+
+# Kill a process
+kill <process_id>
+
+# Check the status of a process
+systemctl status <service_name>
